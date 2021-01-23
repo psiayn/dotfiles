612ea5054bc7f0321c89e34810562a5438410805
Pranav Kesavarapu <pranavkesavarapu@gmail.com>
2021-01-23 09:56:20 +0530

    inamacs

--- init.el
+++ init.el
@@ -1,7 +1,9 @@
-;; melpa package repo
+ ;;; melpa package repo
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
+
+(require 'use-package)
 
 ;; (use-package tron-legacy-theme
 ;;  :config
@@ -17,42 +19,36 @@
   kept-old-versions 5    ; and how many of the old
   )
 
+;; emacs remove autosave
+(setq backup-directory-alist
+    `(("." . ,(concat user-emacs-directory "backups"))))
 
 ;; pdf mode
 (add-hook 'prog-mode-hook 'linum-mode)
 
-;; doom themes
-(use-package doom-themes
+;; challenger-deep
+(use-package challenger-deep-theme
   :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
-    ;; handling theme issues with emacsclient
-    (if (daemonp)
-	(add-hook 'after-make-frame-functions
-	    (lambda (frame)
-		(with-selected-frame frame
-		  (load-theme 'doom-one t)
-		  (toggle-scroll-bar -1))))
-	(load-theme 'doom-one t))
-    (load-theme 'doom-one)
-
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-  
-  ;; Enable custom neotree theme (all-the-icons must be installed!)
-  ;; (doom-themes-neotree-config)
-  ;; or for treemacs users
-  ;; (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
-  ;; (doom-themes-treemacs-config)
-  
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
+  (load-theme 'challenger-deep t))
 
 (require 'evil)
 (evil-mode 1)
 (require 'evil-commentary)
 (evil-commentary-mode)
+
+(require 'telephone-line)
+(setq telephone-line-lhs
+      '((evil   . (telephone-line-evil-tag-segment))
+        (accent . (telephone-line-vc-segment
+                   telephone-line-erc-modified-channels-segment
+                   telephone-line-process-segment))
+        (nil    . (telephone-line-minor-mode-segment
+                   telephone-line-buffer-segment))))
+(setq telephone-line-rhs
+      '((nil    . (telephone-line-misc-info-segment))
+        (accent . (telephone-line-major-mode-segment))
+        (evil   . (telephone-line-airline-position-segment))))
+(telephone-line-mode 1)
 
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
@@ -67,6 +63,7 @@
    ["#1c1e26" "#e95678" "#09f7a0" "#fab795" "#21bfc2" "#6c6f93" "#59e3e3" "#c7c9cb"])
  '(custom-safe-themes
    '("1623aa627fecd5877246f48199b8e2856647c99c6acdab506173f9bb8b0a41ac" "5036346b7b232c57f76e8fb72a9c0558174f87760113546d3a9838130f1cdb74" "d0aa1464d7e55d18ca1e0381627fac40229b9a24bca2a3c1db8446482ce8185e" "74ba9ed7161a26bfe04580279b8cad163c00b802f54c574bfa5d924b99daa4b9" "5b809c3eae60da2af8a8cfba4e9e04b4d608cb49584cb5998f6e4a1c87c057c4" "4f01c1df1d203787560a67c1b295423174fd49934deb5e6789abd1e61dba9552" "3c2f28c6ba2ad7373ea4c43f28fcf2eed14818ec9f0659b1c97d4e89c99e091e" "2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "e72f5955ec6d8585b8ddb2accc2a4cb78d28629483ef3dcfee00ef3745e2292f" "79278310dd6cacf2d2f491063c4ab8b129fee2a498e4c25912ddaa6c3c5b621e" "1db4be958a1df556190253eaee2717c554402f93d96ff6ec9e206567d906817e" default))
+ '(delete-selection-mode nil)
  '(fci-rule-color "#f9cec3")
  '(global-company-mode t)
  '(jdee-db-active-breakpoint-face-colors (cons "#16161c" "#e95678"))
@@ -74,7 +71,7 @@
  '(jdee-db-spec-breakpoint-face-colors (cons "#16161c" "#6a6a6a"))
  '(objed-cursor-color "#e95678")
  '(package-selected-packages
-   '(ox-reveal scala-mode lsp-metals lsp-java rjsx-mode exec-path-from-shell haskell-mode lsp-python-ms w3m pdf-tools lsp-ivy lsp-treemacs lsp-ui lsp-mode rust-mode evil-commentary dashboard page-break-lines treemacs-all-the-icons doom-themes irony vterm zones swiper ivy tron-legacy-theme company-jedi elpy centaur-tabs treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile use-package projectile treemacs-evil treemacs evil))
+   '(telephone-line evil-magit helm lsp-javacomp challenger-deep-theme sbt-mode ox-reveal scala-mode lsp-metals lsp-java rjsx-mode exec-path-from-shell haskell-mode lsp-python-ms w3m pdf-tools lsp-ivy lsp-treemacs lsp-ui lsp-mode rust-mode evil-commentary dashboard page-break-lines treemacs-all-the-icons doom-themes irony vterm zones swiper tron-legacy-theme company-jedi elpy centaur-tabs treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile use-package projectile treemacs-evil treemacs evil))
  '(pdf-view-midnight-colors (cons "#c7c9cb" "#1c1e26"))
  '(rustic-ansi-faces
    ["#1c1e26" "#e95678" "#09f7a0" "#fab795" "#21bfc2" "#6c6f93" "#59e3e3" "#c7c9cb"])
@@ -208,23 +205,73 @@
   :config (treemacs-set-scope-type 'Perspectives))
 
 ;; centaur-tabs
-(require 'centaur-tabs)
-(centaur-tabs-mode t)
-(global-set-key (kbd "C-<prior>")  'centaur-tabs-backward)
-(global-set-key (kbd "C-<next>") 'centaur-tabs-forward)
-(centaur-tabs-headline-match)
-(setq centaur-tabs-style "bar")
-(setq centaur-tabs-set-bar 'under)
-(define-key evil-normal-state-map (kbd "g t") 'centaur-tabs-forward)
-(define-key evil-normal-state-map (kbd "g T") 'centaur-tabs-backward)
+;; (require 'centaur-tabs)
+;; (centaur-tabs-mode t)
+;; (global-set-key (kbd "C-<prior>")  'centaur-tabs-backward)
+;; (global-set-key (kbd "C-<next>") 'centaur-tabs-forward)
+;; (centaur-tabs-headline-match)
+;; (setq centaur-tabs-style "bar")
+;; (setq centaur-tabs-set-bar 'under)
+;; (define-key evil-normal-state-map (kbd "g t") 'centaur-tabs-forward)
+;; (define-key evil-normal-state-map (kbd "g T") 'centaur-tabs-backward)
 
 
 ;; company mode and ivy mode
 (require 'company)
 (global-company-mode)
 
-(require 'ivy)
-(ivy-mode)
+;; helm time
+(require 'helm)
+(require 'helm-config)
+
+;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t
+      helm-echo-input-in-header-line t)
+
+(defun spacemacs//helm-hide-minibuffer-maybe ()
+  "Hide minibuffer in Helm session if we use the header line as input field."
+  (when (with-helm-buffer helm-echo-input-in-header-line)
+    (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+      (overlay-put ov 'window (selected-window))
+      (overlay-put ov 'face
+                   (let ((bg-color (face-background 'default nil)))
+                     `(:background ,bg-color :foreground ,bg-color)))
+      (setq-local cursor-type nil))))
+
+
+(add-hook 'helm-minibuffer-set-up-hook
+          'spacemacs//helm-hide-minibuffer-maybe)
+
+(setq helm-autoresize-max-height 0)
+(setq helm-autoresize-min-height 20)
+(setq helm-M-x-fuzzy-match t)
+(setq helm-buffers-fuzzy-matching t
+      helm-recentf-fuzzy-match    t)
+
+
+(helm-autoresize-mode 1)
+
+(helm-mode 1)
+
 
 ;; irony mode
 (add-hook 'c++-mode-hook 'irony-mode)
@@ -233,63 +280,13 @@
 
 (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
 
-;; mu4e mail
-(require 'mu4e)
-
-;; default
-(setq mu4e-maildir (expand-file-name "~/Maildir"))
-
-(setq mu4e-drafts-folder "/[Gmail].Drafts")
-(setq mu4e-sent-folder   "/[Gmail].Sent Mail")
-(setq mu4e-trash-folder  "/[Gmail].Trash")
-
-;; don't save message to Sent Messages, GMail/IMAP will take care of this
-(setq mu4e-sent-messages-behavior 'delete)
-
-;; setup some handy shortcuts
-(setq mu4e-maildir-shortcuts
-      '(("/INBOX"             . ?i)
-        ("/[Gmail].Sent Mail" . ?s)
-        ("/[Gmail].Trash"     . ?t)))
-
-;; allow for updating mail using 'U' in the main view:
-(setq mu4e-get-mail-command "offlineimap")
-
-;; something about ourselves
-;; I don't use a signature...
-(setq
- user-mail-address "pranavkesavarapu@gmail.com"
- user-full-name  "Pranav Kesavarapu"
- ;; message-signature
- ;;  (concat
- ;;    "Foo X. Bar\n"
- ;;    "http://www.example.com\n")
-)
-
-;; sending mail -- replace USERNAME with your gmail username
-;; also, make sure the gnutls command line utils are installed
-;; package 'gnutls-bin' in Debian/Ubuntu, 'gnutls' in Archlinux.
-
-(require 'smtpmail)
-
-(setq message-send-mail-function 'smtpmail-send-it
-      starttls-use-gnutls t
-      smtpmail-starttls-credentials
-      '(("smtp.gmail.com" 587 nil nil))
-      smtpmail-auth-credentials
-      (expand-file-name "~/.authinfo.gpg")
-      smtpmail-default-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-service 587
-      smtpmail-debug-info t)
-
 ;; setting up dashboard :nezuko_pump:
 (require 'dashboard)
 (dashboard-setup-startup-hook)
 ;; Set the title
-(setq dashboard-banner-logo-title "Welcome to Widemacs")
+(setq dashboard-banner-logo-title "Welcome to Inamacs")
 ;; Set the banner
-(setq dashboard-startup-banner "/home/psiayn/.emacs.d/memes/wide.png")
+(setq dashboard-startup-banner "/home/psiayn/.emacs.d/images/ina.png")
 ;; Value can be
 ;; 'official which displays the official emacs logo
 ;; 'logo which displays an alternative emacs logo
@@ -303,10 +300,10 @@
 ;; (setq dashboard-show-shortcuts nil)
 
 (setq dashboard-items '((recents  . 5)
-                        (bookmarks . 5)
-                        (projects . 5)
-                        (agenda . 5)
-                        (registers . 5)))
+			(bookmarks . 5)
+			(projects . 5)
+			(agenda . 5)
+			(registers . 5)))
 
 ;; rust-mode and lsp
 (require 'rust-mode)
@@ -320,20 +317,41 @@
 (setq lsp-python-ms-auto-install-server t)
 (add-hook 'python-mode-hook #'lsp)
 
-;; haskell-mode
-(require 'haskell-mode)
-(require 'haskell-process)
-(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-
 ;; java go brrr
 (require 'lsp-java)
 (add-hook 'java-mode-hook #'lsp)
 
 ;; scala
+;; Enable sbt mode for executing sbt commands
+(use-package sbt-mode
+  :commands sbt-start sbt-command
+  :config
+  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
+  ;; allows using SPACE when in the minibuffer
+  (substitute-key-definition
+   'minibuffer-complete-word
+   'self-insert-command
+   minibuffer-local-completion-map)
+   ;; sbt-supershell kills sbt-mode:  https://github.com/hvesalai/emacs-sbt-mode/issues/152
+   (setq sbt:program-options '("-Dsbt.supershell=false"))
+)
+
+;; Enable nice rendering of diagnostics like compile errors.
+(use-package flycheck
+  :init (global-flycheck-mode))
+
+(use-package lsp-mode
+  ;; Optional - enable lsp-mode automatically in scala files
+  :hook  (scala-mode . lsp)
+         (lsp-mode . lsp-lens-mode)
+  :config (setq lsp-prefer-flymake nil))
+
+;; Add metals backend for lsp-mode
 (use-package lsp-metals
   :config (setq lsp-metals-treeview-show-when-views-received t))
+
+;; Enable nice rendering of documentation on hover
 (use-package lsp-ui)
-(use-package yasnippet)
 
 ;; Use the Debug Adapter Protocol for running tests and debugging
 (use-package posframe
@@ -347,5 +365,5 @@
 
 ;; org reveal.js
 (require 'ox-reveal)
-(setq Org-Reveal-root "file:///home/psiayn/Downloads/reveal.js-master")
+(setq Org-Reveal-root "file:///home/psiayn/Downloads/reveal.js-4.1.0")
 (setq Org-Reveal-title-slide nil)
